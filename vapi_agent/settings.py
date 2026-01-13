caa6ccdb8e01a66f193bfdb3f7b58ea2871db222
abhaya <gautamabhaya98@gmail.com>
2026-01-13 23:34:09 +0545

    chore: set up env vars and installed vapi server sdk

--- vapi_agent/settings.py
+++ vapi_agent/settings.py
@@ -10,17 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR/ ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t*c=7=-hd7aaj62go3uy(gn*k6z(c=2u$18e&fb*5nk$5rhu+n'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+VAPI_PUBLIC_KEY = os.getenv('VAPI_PUBLIC_KEY')
